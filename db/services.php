a97dfa5b70565e66ef26b6cf469e69d6b82fcfd0
justinhunt <bitwalkerjapan@gmail.com>
2017-01-11 15:53:33 +0900

    Initial commit of Moodle Zapier integration

---
+++ db/services.php
@@ -0,0 +1,42 @@
+<?php
+
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Web service local plugin template external functions and service definitions.
+ *
+ * @package    localzapier
+ * @copyright  2011 Jerome Mouneyrac
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// We defined the web service functions to install.
+$functions = array(
+        'local_zapier_hello_world' => array(
+                'classname'   => 'local_zapier_external',
+                'methodname'  => 'hello_world',
+                'classpath'   => 'local/zapier/externallib.php',
+                'description' => 'Return Hello World FIRSTNAME. Can change the text (Hello World) sending a new text as parameter',
+                'type'        => 'read',
+        )
+);
+
+// We define the services to install as pre-build services. A pre-build service is not editable by administrator.
+$services = array(
+        'My service' => array(
+                'functions' => array ('local_zapier_hello_world'),
+                'restrictedusers' => 0,
+                'enabled'=>1,
+        )
+);
