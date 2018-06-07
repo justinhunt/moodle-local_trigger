56b197f4072ba1e72b6dc3c5c8ef3ae3109c9dc0
justinhunt <bitwalkerjapan@gmail.com>
2018-06-07 23:05:27 +0900

    removed web services and fixed biug where a webhook could be called more than once for a single event firing

--- db/events.php
+++ db/events.php
@@ -27,12 +27,18 @@ defined('MOODLE_INTERNAL') || die();
 
 $config = get_config('local_trigger');
 $observers = array();
+$added = array();
 
 $webhooks = \local_trigger\webhook\webhooks::fetch_items();
-foreach($webhooks as $webhook){
-    $observers[] = array(
-        'eventname' => $webhook->event,
-        'callback' => '\local_trigger\event_trigger::trigger',
-        'internal' => false
-    );
+foreach($webhooks as $webhook) {
+    //we only want to add each event once, even if multiple trigger webhooks for it are registered
+    //when it fires we loop through the registered hooks
+    if (!array_key_exists($webhook->event, $added)) {
+        $added[$webhook->event]=true;
+        $observers[] = array(
+            'eventname' => $webhook->event,
+            'callback' => '\local_trigger\event_trigger::trigger',
+            'internal' => false
+        );
+    }
 }
