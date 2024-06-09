61b82be64ca1a6939edf14d6604bfe4b6c05d6e0
Justin Hunt <bitwalkerjapan@gmail.com>
2024-06-09 17:09:07 +0900

    tweaks to get_custom_actions

--- externallib.php
+++ externallib.php
@@ -702,11 +702,15 @@ class local_trigger_services extends external_api {
             //$customactions = ['error'=>$exception->getMessage()];
             $customactions = [];
         }
-        return json_encode($customactions,JSON_UNESCAPED_SLASHES);
+        return $customactions;
      }
 
     public static function get_customactions_returns() {
-        return new external_value(PARAM_RAW);
+        return new external_single_structure(array(
+                    'id' => new external_value(PARAM_STRINGID, 'action id'),
+                    'action' => new external_value(PARAM_RAW, 'action name'))
+            );
+        //return new external_value(PARAM_RAW);
     }
 
     /**
